171a790d987bc0c15559674d266a6d2f2b86da9a
lostlang <53492781+lostlang@users.noreply.github.com>
2021-09-08 13:21:37 +0300

    DataClasses complete

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -2,10 +2,38 @@ package ru.sber.oop
 
 data class User(val name: String, val age: Long) {
     lateinit var city: String
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is User) return false
+
+        val out = this.name == other.name && this.age == other.age
+
+        if (!out) {
+            return false
+        }
+
+        val thisHaveCity = this::city.isInitialized
+        val otherHaveCity = other::city.isInitialized
+
+        if (thisHaveCity != otherHaveCity){
+            return false
+        }
+
+        if (thisHaveCity && city != other.city) {
+            return false
+        }
+
+        return out
+    }
+
 }
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    user1.city = "Omsk"
+    val user2 = user1.copy(name = "Alec")
+    val user3 = user1.copy()
+    user3.city = "Tomsk"
+
+    println(user1 == user3)
 }
