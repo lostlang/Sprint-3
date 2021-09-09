58fdb8e3642a0b2562aa96a14b751574cacfc80c
lostlang <53492781+lostlang@users.noreply.github.com>
2021-09-10 00:25:26 +0300

    Inheritance complete

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,13 +1,22 @@
 package ru.sber.oop
 
-class Room(val name: String, val size: Int) {
+open class Room(val name: String, val size: Int) {
 
-    val dangerLevel = 5
+    protected open val dangerLevel = 5
 
     fun description() = "Room: $name"
 
-    fun load() = "Nothing much to see here..."
+    open fun load() = "Nothing much to see here..."
+
+    fun default(): Room {
+        return Room( "Bedroom", 100)
+    }
 
 }
 
-//TODO: create class TownSquare here...
+class TownSquare(): Room("Town Square", 1000){
+
+    override val dangerLevel = super.dangerLevel - 3
+    override fun load() = "King $name"
+
+}
