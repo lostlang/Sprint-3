559e5036a1773c830a07571ff0105ecc8a2105e7
lostlang <53492781+lostlang@users.noreply.github.com>
2021-09-16 08:45:15 +0300

    Inheritance update

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,22 +1,20 @@
 package ru.sber.oop
 
 open class Room(val name: String, val size: Int) {
-
     protected open val dangerLevel = 5
+    var enemy: Monster = Goblin("Flin", "bed joke", "fire", 50)
 
     fun description() = "Room: $name"
 
-    open fun load() = "Nothing much to see here..."
+    open fun load() = this.enemy.getSalutation()
 
     fun default(): Room {
         return Room( "Bedroom", 100)
     }
-
 }
 
 class TownSquare(): Room("Town Square", 1000){
-
     override val dangerLevel = super.dangerLevel - 3
+    
     override fun load() = "King $name"
-
 }
