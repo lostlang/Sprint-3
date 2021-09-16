1a704e47c85c3eacbdd78173bca135af29a8a8ba
lostlang <53492781+lostlang@users.noreply.github.com>
2021-09-16 08:38:40 +0300

    Polymorphism complete

--- oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
+++ oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
@@ -4,10 +4,36 @@ interface Fightable {
     val powerType: String
     var healthPoints: Int
     val damageRoll: Int
+        get() = (1..10).random()
 
     fun attack(opponent: Fightable): Int
 }
 
-//TODO: create class Player, Monster, Goblin here...
+class Player(var name: String, var isBlessed: Boolean,
+             override val powerType: String, override var healthPoints: Int): Fightable {
+    override fun attack(opponent: Fightable): Int {
+        val damage = if (this.isBlessed) this.damageRoll * 2 else this.damageRoll
+        opponent.healthPoints -= damage
+        return damage
+    }
 
+}
 
+abstract class Monster(): Fightable {
+    abstract val name: String
+    abstract val description: String
+
+    override fun attack(opponent: Fightable): Int {
+        val damage = this.damageRoll
+        opponent.healthPoints -= damage
+        return damage
+    }
+}
+
+class Goblin(override val name: String, override val description: String,
+             override val powerType: String, override var healthPoints: Int) : Monster(){
+    override val damageRoll: Int
+        get() = super.damageRoll / 2
+}
+
+fun Monster.getSalutation() = "Hello, I'm ${this.name}"
